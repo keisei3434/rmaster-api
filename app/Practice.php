924d564a28ad1d63c8893e55daa5191436da29e2
keisei.matsumoto <keisei3434@gmail.com>
2020-08-12 01:26:17 +0000

    modify practice.php

--- app/Practice.php
+++ app/Practice.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Practice extends Model
 {
-    protected $fillable = ['place_id', 'start_at', 'end_at'];
+    protected $fillable = ['place_id', 'is_active', 'players', 'start_at', 'end_at'];
 }
